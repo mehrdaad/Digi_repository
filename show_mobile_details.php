73bf6670476c10369dfb84e05a4a2aa970640a4f
shirin <tahmasebi_shirin@yahoo.com>
2015-11-06 13:47:33 -0800

    Show Mobile Details

---
+++ show_mobile_details.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<?php include 'head.php'; ?>
+<?php include 'header.php'; ?>
+
+<body>
+<?php 
+$admin=false;
+if($admin)include 'admin-menu.php'; ?>
+    <!-- MENU SECTION END-->
+
+    <div class="content-wrapper">
+           <div class="container">
+                <div class="row">
+                <div class="col-md-4">
+                  <!--   Kitchen Sink -->
+                    
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered table-hover">
+                                    
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>سیستم عامل</td>
+                                            <td>اندروید</td>
+                                        </tr>
+                                        <tr>
+                                            <td>2</td>
+                                            <td>برند گوشی</td>
+                                            <td>سونی</td>
+                                        </tr>
+                                        <tr>
+                                            <td>3</td>
+                                            <td>تعداد سیم کارت</td>
+                                            <td>1</td>
+                                        </tr>
+                                        <tr>
+                                            <td>4</td>
+                                            <td>وزن گوشی</td>
+                                            <td>...</td>
+                                        </tr>
+                                        <tr>
+                                            <td>5</td>
+                                            <td>اندازه گوشی</td>
+                                            <td>...</td>
+                                        </tr>
+                                         <tr>
+                                            <td>6</td>
+                                            <td>حافظه داخلی گوشی</td>
+                                            <td>...</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>7</td>
+                                            <td>سایر توضیحات</td>
+                                            <td>...</td>
+                                        </tr>
+
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    
+                     <!-- End  Kitchen Sink -->
+                </div>
+            </div>
+        </div>
+</div>
+
+    <!-- CONTENT-WRAPPER SECTION END-->
+
+
+
+    <?php include 'footer.php'; ?>
+    <!-- FOOTER SECTION END-->
+    <!-- JAVASCRIPT AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+    <!-- CORE JQUERY SCRIPTS -->
+   
+</body>
+</html>

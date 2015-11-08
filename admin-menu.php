090a59cfeba63f6b505d057111189c57c08d23b8
shirin <tahmasebi_shirin@yahoo.com>
2015-11-07 20:25:49 -0800

    Admin Menu Items Changed

--- admin-menu.php
+++ admin-menu.php
@@ -5,12 +5,10 @@
         <div class="navbar-collapse collapse ">
           <ul id="menu-top" class="nav navbar-nav navbar-right">
             <li><a class="menu-top-active" href="admin-page.php">پنل مخصوص ادمین</a></li>
-            <li><a href="addlaptop.php">add laptop</a></li>
-            <li><a href="addmobile.php">add phone directly</a></li>
-            <li><a href="showconfirmedstauff.php">buying states directly</a></li>
-            <li><a href="index.php">main page</a></li>
-            <li><a href="orderedstuff.php">ordered stuffs</a></li>
-            <li><a href="addstaff.php">add stuf</a></li>
+            
+            <li><a href="showconfirmedstauff.php">گزارش کالاهای تایید شده</a></li>
+            <li><a href="orderedstuff.php">اجناس سفارش داده شده</a></li>
+            <li><a href="addstaff.php">اضافه کردن کالا</a></li>
           </ul>
         </div>
       </div>
